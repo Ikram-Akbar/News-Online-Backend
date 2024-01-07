75d1e80337c286223534af8968f7d800e5abf572
ikram akbar <ikramakbar71@gmail.com>
2024-01-07 18:23:25 +0600

    Create News And Category Data Loading API

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require('cors');
-const categories = require('./fakeDB/categories.json');
+const cors = require("cors");
+const categories = require("./fakeDB/categories.json");
 const news = require("./fakeDB/news.json");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World!" });
 });
 
 app.get("/categories", (req, res) => {
-    res.status(200).send(categories)
-})
+    res.status(200).send(categories);
+});
+app.get("/categories/:id", (req, res) => {
+    const id = req.params.id;
+
+    if (id == 0) {
+        res.send(news);
+    } else {
+        const result = categories.filter((c) => c.id === id);
+        res.send(result);
+    }
+});
 
 app.get("/news", (req, res) => {
-    res.status(200).send(news)
-})
+    res.status(200).send(news);
+});
+app.get("/news/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find((n) => n._id === id);
+    if (!selectedNews) {
+        res.status(404).send({ message: "News not found" });
+    } else {
+        res.send(selectedNews);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
